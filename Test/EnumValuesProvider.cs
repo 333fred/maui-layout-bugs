b923ae0123708366d729ed36443668585c4d992b
Fredric Silberberg <fred@silberberg.xyz>
2022-10-09 23:28:46 -0700

    Example of IMarkupExtenstion bug

---
+++ Test/EnumValuesProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Test
+{
+    public class EnumValuesProvider<T> : IMarkupExtension<T[]> where T : struct, Enum
+    {
+        public T[] ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues<T>();
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
--- Test/MainPage.xaml.cs
+++ Test/MainPage.xaml.cs
@@ -7,18 +7,5 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        BindingContext = new MainPageViewModel();
     }
-}
-
-public class MainPageViewModel
-{
-    public ObservableCollection<CardViewModelCollection> Matches { get; } = new ObservableCollection<CardViewModelCollection>(Enumerable.Range(0, 20).Select(_ => new CardViewModelCollection()));
-}
-
-public class CardViewModelCollection
-{
-    public List<CardViewModel> Cards { get; } = Enumerable.Range(1, 6).Select(i => new CardViewModel(i)).ToList();
-    // Comment the above and uncomment this to null ref the xaml compiler
-    //public CardViewModel[] Cards { get; } = Enumerable.Range(1, 6).Select(i => new CardViewModel(i)).ToArray();
 }
